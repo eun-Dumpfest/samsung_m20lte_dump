98169e53eb93cb95cb32c1dd2bee00c8141dc490
root <root@fv-az83-275.pawgbth4qraerhbqawdeo1jfng.dx.internal.cloudapp.net>
2022-06-27 13:01:51 +0000

    Add extras for havoc_m20lte-userdebug 11 RQ3A.211001.001 1650622175 release-keys
    Signed-off-by: root <root@fv-az83-275.pawgbth4qraerhbqawdeo1jfng.dx.internal.cloudapp.net>

---
+++ README.md
@@ -0,0 +1,21 @@
+## havoc_m20lte-userdebug 11 RQ3A.211001.001 1650622175 release-keys
+- Manufacturer: samsung
+- Platform: universal7904
+- Codename: m20lte
+- Brand: samsung
+- Flavor: havoc_m20lte-userdebug
+- Release Version: 11
+- Id: RQ3A.211001.001
+- Incremental: 1650622175
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: samsung/havoc_m20lte/m20lte:11/RQ3A.211001.001/alif04221008:userdebug/release-keys
+- OTA version: 
+- Branch: havoc_m20lte-userdebug-11-RQ3A.211001.001-1650622175-release-keys
+- Repo: samsung_m20lte_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/product/priv-app/RecorderPrebuilt/RecorderPrebuilt.apk.* 2>/dev/null >> system/system/product/priv-app/RecorderPrebuilt/RecorderPrebuilt.apk
+rm -f system/system/product/priv-app/RecorderPrebuilt/RecorderPrebuilt.apk.* 2>/dev/null
+cat system/system/product/priv-app/PrebuiltGmsCore/PrebuiltGmsCoreRvc.apk.* 2>/dev/null >> system/system/product/priv-app/PrebuiltGmsCore/PrebuiltGmsCoreRvc.apk
+rm -f system/system/product/priv-app/PrebuiltGmsCore/PrebuiltGmsCoreRvc.apk.* 2>/dev/null
+cat system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/product/priv-app/Velvet/Velvet.apk
+rm -f system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/product/app/Photos/Photos.apk.* 2>/dev/null >> system/system/product/app/Photos/Photos.apk
+rm -f system/system/product/app/Photos/Photos.apk.* 2>/dev/null
+cat system/system/product/app/LatinIMEGooglePrebuilt/LatinIMEGooglePrebuilt.apk.* 2>/dev/null >> system/system/product/app/LatinIMEGooglePrebuilt/LatinIMEGooglePrebuilt.apk
+rm -f system/system/product/app/LatinIMEGooglePrebuilt/LatinIMEGooglePrebuilt.apk.* 2>/dev/null
